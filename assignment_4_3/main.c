52aeed50341a33134d90b3a97a3bde1024ce16a1
Olav Liljeberg <omliljeber@uia.no>
2020-10-29 13:56:31 -0600

    Added assignment 4

---
+++ assignment_4_3/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LIMIT 100
+
+int main()
+{
+    char str1[MAX_LIMIT];
+    char str2[MAX_LIMIT];
+    // ask for the first string
+    printf("Enter first string: \n");
+    scanf("%s", str1);
+    // ask for the second string
+    printf("Enter second string here: \n");
+    scanf("%s", str2);
+
+    if(!strcmp(str1,str2))    // If the comparison is 0, then the strings are equal
+        printf("The strings are equal\n");
+    else
+        printf("The strings are not equal\n");
+
+    char * pFirstString, *pSecondString; // declare 2 char pointers
+    pFirstString = strstr(str2, str1);   // return pointer to first char that matches string 1 in string 2
+    pSecondString= strstr(str1, str2);  // return pointer to first char that matches string 2 in string 1
+    if(pFirstString || pSecondString)   // if either is a substring og the other then it's a substring
+        printf("It is a substring!\n"); 
+    else
+        printf("It is not a substring!\n");
+
+    return 0;
+}
